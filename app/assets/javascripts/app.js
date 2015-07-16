7d8546fc25c65680aad5be6a0dd407ba47f8ec41
Fernando Lucero <Fernando@Fernandos-MacBook-Pro.local>
2015-07-17 09:05:20 +1000

    refactoring to game.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,7 +3,7 @@ window.onload = function() {
   if ( $("#gameContainer").length > 0 ) {
     var game = new Phaser.Game(1000, 700, Phaser.AUTO, '#gameContainer');
 
-    //  Add the States to game has.
+    //  Add the States the game has.
     //  Don't have to do this in the html, it could be done in the Boot state too.
     //  As each state gets started it will call the next state.
     game.state.add('Boot', BasicGame.Boot);
